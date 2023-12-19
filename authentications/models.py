479d759ec8aefc2bf28a3ec7540e8773d0b1cd97
KaitoMizukami <kaito.program@gmail.com>
2023-12-19 22:40:54 +0800

    Add User model and manager class

--- authentications/models.py
+++ authentications/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Email address is required')
+        if not username:
+            raise ValueError('Username is required')
+        
+        email = self.normalize_email(email)
+        user = self.model(
+            username=username,
+            email=email,  
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        super_user = self.create_user(email, username, password)
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        super_user.save(using=self._db)
+        return super_user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User model schema
+    """
+    email = models.EmailField(
+        max_length=250, unique=True,
+        verbose_name='email address'
+    )
+    username = models.CharField(max_length=100, verbose_name='username')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
