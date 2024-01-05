97f1ffa590ce28180b1cf46e9ac9bca55e42cbb7
KaitoMizukami <kaito.program@gmail.com>
2024-01-05 21:06:43 +0800

    Customize for our own user model

--- authentications/admin.py
+++ authentications/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'username', 'is_staff')
+    fieldsets = (
+        ('ユーザー情報', {
+            'fields': (
+                'username', 'email', 'password'
+            )
+        }),
+        ('パーミッション', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser'
+            )
+        }),
+    )
+
+    add_fieldsets = (
+        ('ユーザー情報', {
+            'fields': (
+                'username','email', 'password', 'confirm_password'
+            )
+        }),
+    )
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active',)
+
+    search_fields = ('email', 'username',)
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
+admin.site.unregister(Group) 
